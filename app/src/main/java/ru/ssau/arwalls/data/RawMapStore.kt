36bcd177f06efc4168bf0ed3add2af0a43ae0346
Fomin <dortan_mors@mail.ru>
2024-05-16 02:59:20 +0400

    fixed map matrix concurrent modification

--- app/src/main/java/ru/ssau/arwalls/data/RawMapStore.kt
+++ app/src/main/java/ru/ssau/arwalls/data/RawMapStore.kt
@@ -9,6 +9,8 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import ru.ssau.arwalls.common.Settings
 import ru.ssau.arwalls.common.tag
 import ru.ssau.arwalls.rawdepth.FloatsPerPoint
@@ -18,6 +20,7 @@ object RawMapStore {
     private const val mapScale = 100
 
     private var rawMatrix = BitMatrix()
+    val mutex = Mutex()
 
     private val coroutineScope = CoroutineScope(
         Dispatchers.IO +
@@ -30,30 +33,35 @@ object RawMapStore {
         coroutineScope.launch {
             val pointsArray = mapState.points.array()
             with(rawMatrix) {
-                for (i in pointsArray.indices step FloatsPerPoint) {
-                    if (pointsArray[i + 1] - Settings.heightOffset in -Settings.scanVerticalRadius..Settings.scanVerticalRadius) { // Y
-                        set(
-                            x = (pointsArray[i] * mapScale).toInt(),     // X
-                            y = (pointsArray[i + 2] * mapScale).toInt(), // Z
-                        )
+                mutex.withLock {
+                    for (i in pointsArray.indices step FloatsPerPoint) {
+                        if (pointsArray[i + 1] - Settings.heightOffset in -Settings.scanVerticalRadius..Settings.scanVerticalRadius) { // Y
+                            set(
+                                x = (pointsArray[i] * mapScale).toInt(),     // X
+                                y = (pointsArray[i + 2] * mapScale).toInt(), // Z
+                            )
+                        }
                     }
                 }
             }
         }
     }
 
-    fun getBitmap(): Bitmap =
+    suspend fun getBitmap(): Bitmap =
         with(rawMatrix) {
-            val maxX = filledPoints.maxOf { (x, _) -> x }
-            val minX = filledPoints.minOf { (x, _) -> x }
-            val maxY = filledPoints.maxOf { (_, y) -> y }
-            val minY = filledPoints.minOf { (_, y) -> y }
+            val points = mutex.withLock {
+                filledPoints.toMutableList().toList()
+            }
+            val maxX = points.maxOf { (x, _) -> x }
+            val minX = points.minOf { (x, _) -> x }
+            val maxY = points.maxOf { (_, y) -> y }
+            val minY = points.minOf { (_, y) -> y }
             val offsetX = 0 - minX
             val offsetY = 0 - minY
             val width = maxX - minX + 1
             val height = maxY - minY + 1
             Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565).apply {
-                filledPoints.forEach { (x, y) ->
+                points.forEach { (x, y) ->
                     setPixel(x + offsetX, y + offsetY, Color.BLACK)
                 }
             }
