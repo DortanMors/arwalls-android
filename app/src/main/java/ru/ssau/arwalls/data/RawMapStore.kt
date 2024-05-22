25fa6b08ad108a93276a33bc216d832b360c2ca2
Fomin <dortan_mors@mail.ru>
2024-05-22 20:04:20 +0400

    map scaling

--- app/src/main/java/ru/ssau/arwalls/data/RawMapStore.kt
+++ app/src/main/java/ru/ssau/arwalls/data/RawMapStore.kt
@@ -3,48 +3,50 @@ package ru.ssau.arwalls.data
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.Matrix
 import android.graphics.Paint
 import android.graphics.Path
-import android.util.Log
-import kotlinx.coroutines.CoroutineExceptionHandler
-import kotlinx.coroutines.CoroutineName
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.sync.Mutex
-import ru.ssau.arwalls.common.tag
+import android.graphics.RectF
+import ru.ssau.arwalls.common.Settings
 import ru.ssau.arwalls.ui.model.MapState
 
 
 object RawMapStore {
-    private const val mapScale = 100
-
     private var rawMatrix = BitMatrix()
-    val mutex = Mutex()
     private var path = Path()
-
-    private val coroutineScope = CoroutineScope(
-        Dispatchers.IO +
-            Job() +
-            CoroutineName("RawMapStore") +
-            CoroutineExceptionHandler { _, throwable -> Log.e(tag, throwable.toString()) }
-    )
+    private val bitmapPaint = Paint().apply {
+        style = Settings.paintStyle
+        strokeWidth = Settings.strokeWidth
+        color = Color.BLACK
+    }
 
     fun updateMapStateAsync(mapState: MapState) {
         path = mapState.path
     }
 
-    suspend fun getBitmap(): Bitmap =
+    fun getBitmap(): Bitmap =
         Bitmap.createBitmap(1000, 1000, Bitmap.Config.RGB_565).apply {
             eraseColor(Color.WHITE)
-            Canvas(this).drawPath(
-                path,
-                Paint().apply {
-                    color = Color.BLACK
-                    strokeWidth = 10f
-                },
-            )
+            drawScaledPathOnBitmap(path, this)
         }
+
+    private fun drawScaledPathOnBitmap(path: Path, bitmap: Bitmap) {
+        val pathBounds = RectF()
+        path.computeBounds(pathBounds, true)
+        val scaleX = bitmap.width / pathBounds.width()
+        val scaleY = bitmap.height / pathBounds.height()
+        val scale = minOf(scaleX, scaleY)
+        val dx = (bitmap.width - pathBounds.width() * scale) / 2f - pathBounds.left * scale
+        val dy = (bitmap.height - pathBounds.height() * scale) / 2f - pathBounds.top * scale
+
+        val matrix = Matrix().apply {
+            setScale(scale, scale)
+            postTranslate(dx, dy)
+        }
+        path.transform(matrix)
+
+        Canvas(bitmap).drawPath(path, bitmapPaint)
+    }
 
     fun clear() {
         rawMatrix = BitMatrix()
